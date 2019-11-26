88c778bf89591a140d6a7c602c2c0a0f905c22f1
1980shiet <walsh09chris20@hotmail.com>
2019-11-25 17:20:42 -0800

    Add content front page, journal and styling

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -37,22 +37,7 @@ get_header(); ?>
             <?php endif; ?>
          </section>
 
-			<section class="fp-journal-section">
-				<h2>Inhabitent Journal</h2>
-				<?php
-				$args = array('post_type'=> 'post', 'order' => 'ASC', 'posts_per_page' => 3 );
-				$posts_query = get_posts( $args );
-				?>
-				<div class="fp-journals">
-					<?php foreach ( $posts_query as $post ) : setup_postdata($post) ?>
-						<article class="fp-journal">
-							<?php the_post_thumbnail('medium'); ?>
-							<h3><?php the_title();?></h3>
-							<a href="<?php echo get_the_permalink(); ?>">Read Entry</a>
-						</article>
-					<?php endforeach; wp_reset_postdata(); ?>
-				</div>
-			</section>
+		
 			
 			<section class="latest-advens">
 				<h2>Latest Adventures</h2>
