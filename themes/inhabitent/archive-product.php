478a8cc8d306c06b8bb527f4543a3e9d07e8947e
1980shiet <walsh09chris20@hotmail.com>
2019-11-26 18:18:28 -0800

    Styling shop items

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="product-gallery" role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
 
 <?php get_template_part( 'template-parts/content' ); ?>
