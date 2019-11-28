8250202f9789ac30c7f19c7d543d7376637d58b2
1980shiet <walsh09chris20@hotmail.com>
2019-11-28 11:41:17 -0800

    Finished search button

--- themes/inhabitent/js/script.js
+++ themes/inhabitent/js/script.js
@@ -1,5 +1,23 @@
 (function($) {
-  $('body').append('hello world');
+  $(function() {
+    const searchBtn = document.getElementById('searchbtn');
+    const searchField = document.getElementById('search-input');
+    searchField.style.display = 'none';
 
+    console.log(searchBtn);
+
+    $('#searchbtn').on('click', function() {
+      event.preventDefault();
+      if (searchField.style.display === 'none') {
+        searchField.style.display = 'block';
+      } else {
+        searchField.style.display = 'none';
+      }
+      //console.log('helo');
+    });
+
+    // console.log('hello');
+    // $('body').append('hello world');
+  });
   // todo try to get the search form in the header to open and close on click
 })(jQuery);
